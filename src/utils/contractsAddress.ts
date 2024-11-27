8778db90f15d133e6a9365c9d77bceb3e43044f7
hammasxd <hammas.ali@vaivaltech.com>
2024-11-27 22:01:37 +0500

    new contract

--- src/utils/contractsAddress.ts
+++ src/utils/contractsAddress.ts
@@ -1,2 +1,2 @@
-export const transitContractAddress = '0x3E5e0eb7f7B746fCde368A0351A9ABb7D45a06b0';
+export const transitContractAddress = '0x0735129B455466B6Bff602b7d7A195AE75eE2c40';
 export const adminAddress = '0xe70B9d16945014fdb4388381A89Bbf16039eB29f'
